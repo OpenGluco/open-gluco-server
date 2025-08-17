fa0b42c3e2b6c04e6ea6387ed2ce40047ab3be3c
Lilierd 42 <lilierdyt@gmail.com>
2025-08-17 16:23:43 +0200

    GET /CGMData fully functional

--- api/routes/CGMData.py
+++ api/routes/CGMData.py
@@ -12,7 +12,22 @@ db_conn = get_conn()
 @token_required
 def cgm_data(payload):
     try:
+        period = request.args.get("period")
+
         data = read_from_influx(f"{payload["user_id"]}", "glucose")
+
+        match period:
+            # day case already managed
+            case "w":
+                data = read_from_influx(
+                    f"{payload["user_id"]}", "glucose", 168)
+            case "m":
+                data = read_from_influx(
+                    f"{payload["user_id"]}", "glucose", 5040)
+            case "y":
+                data = read_from_influx(
+                    f"{payload["user_id"]}", "glucose", 1839600)
+
         return jsonify({"message": f"CGM Data for user {payload['user_id']}", "data": data}), 200
 
     except Exception as e:
